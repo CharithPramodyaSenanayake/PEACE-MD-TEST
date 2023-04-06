17ee622196bba191e900cc2d130e00a9a59dfea1
RATHI <102493321+CharithPramodyaSenanayake@users.noreply.github.com>
2023-04-06 12:25:01 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,27 +18,27 @@ global.ANTI_BADWORD = 'true' // true of false
 global.AUTO_REACT = 'false' // true of false
 global.INBOX_BLOCK_MSG ='BLOCK !!!'
 global.INBOX_BLOCK = 'off' //inbox block
-global.alivemsg = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ʙᴏᴛ'
-global.alivepic = 'https://i.ibb.co/zFYBPcQ/IMG-20230226-WA0115.jpg '
-global.owner = ['0786802371'] //ur owner number
-global.ownername = "kaveesha" //ur owner name
+global.alivemsg = 'ᴘᴇᴀᴄᴇ ᴍᴅ'
+global.alivepic = 'https://te.legra.ph/file/7cc522af6b9c0aeb8b62d.jpg'
+global.owner = ['94712448370'] //ur owner number
+global.ownername = "CHARITH" //ur owner name
 global.ytname = "YT: Dark" //ur yt chanel name
 global.socialm = "GitHub: Dark" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
 global.session = " " //session Id
-global.ownernomer = "94786802371" //ur number
+global.ownernomer = "94712448370" //ur number
 global.lang = "SI" //Select Language "EN" to English "SI" to sinhala 
-global.premium = ['94786802371'] //ur premium number
-global.botname = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ᴡʜᴀᴛʜᴀᴘᴘ ʙᴏᴛ' //ur bot name
-global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://www.darknero.ga/' //script link
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Dark Nero By" //ur sticker watermark packname
-global.author = "Dark Nero Creating By Cyber Yakuza Team" //ur sticker watermark author
-global.wm = "Dark Bot Inc." //ur watermark
+global.premium = ['94712448370'] //ur premium number
+global.botname = 'ᴘᴇᴀᴄᴇ ᴍᴅ' //ur bot name
+global.linkz = "https://chat.whatsapp.com/Ca6V8xABBhJIdfPVokJpZ8" //your theme url which will be displayed on whatsapp
+global.websitex = "" //ur website to be displayed
+global.botscript = '' //script link
+global.themeemoji = "✌" //ur theme emoji
+global.packname = "ᴘᴇᴀᴄᴇ ᴍᴅ" //ur sticker watermark packname
+global.author = " ʙʏ ᴄʜᴀʀɪᴛʜ" //ur sticker watermark author
+global.wm = "ᴘᴇᴀᴄᴇ ᴍᴅ" //ur watermark
 
 // Other
 global.sessionName = 'session'
